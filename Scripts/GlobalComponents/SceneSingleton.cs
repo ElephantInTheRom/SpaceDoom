6dbc52df2e7e6c62e4a6a66c514772428c3fe9ba
Elephant <haydens.other.one@gmail.com>
2021-04-04 18:15:28 -0400

    615

---
+++ Scripts/GlobalComponents/SceneSingleton.cs
@@ -0,0 +1,14 @@
+using System;
+using Godot;
+
+using SpaceDoom.Systems.Combat;
+using SpaceDoom.Scenes;
+
+/// <summary>
+/// A singleton for keeping track of the current scene/level and information about those scenes.
+/// Also keeps instances of layers for projectiles.
+/// </summary>
+public static class SceneSingleton
+{
+
+}
